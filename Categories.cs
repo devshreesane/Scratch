76647a212a88597201eea567043d1d790441915b
devshreesane <devshreesane@users.noreply.github.com>
2014-09-07 22:00:46 -0700

    Create Categories.cs

---
+++ Categories.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    class Categories
+    {
+        Dictionary<string, Regex> labelRegexes = new Dictionary<string, Regex>();
+
+        public Categories()
+        {
+            Regex food = new Regex(@".*(trader|fred|safeway|mayuri).*", RegexOptions.Compiled);
+            labelRegexes.Add("Grocery", food);
+        }
+
+        public string[] Categorize(string description)
+        {
+            List<string> categories = new List<string>();
+            foreach (KeyValuePair<string, Regex> pair in labelRegexes)
+            {
+                if(pair.Value.IsMatch(description.ToLower().Trim()))
+                {
+                    categories.Add(pair.Key);
+                }
+            }
+            return categories.ToArray();
+        }
+
+        /*public static void Main(string[] args)
+        {
+            string test1 = "POS Transaction 345600147887 CHIPOTLE 0469          BELLEVUE     WAUS";
+            string test2 = "POS Transaction 14-8019149213 MAYURI FOODS AND VIDEO REDMOND      WAUS";
+            Categories cats = new Categories();
+
+            Console.WriteLine(String.Join(",",cats.Categorize(test1)));
+            Console.WriteLine(String.Join(",",cats.Categorize(test2)));
+        }*/
+    }
+}
