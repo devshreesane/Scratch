049e5a2ae90a9d54439096f6f136efa2716d0438
devshreesane <devshreesane@users.noreply.github.com>
2014-09-10 22:22:01 -0700

    Update Parser.cs

--- Parser.cs
+++ Parser.cs
@@ -13,17 +13,25 @@ namespace Code
             string file = @"C:\Users\devsane\Downloads\HistoryDownloadCredit.csv";
             string[] cols = { "\"Description\"", "\"Amount\"" };
 
-            double totalGr = 0;
+            Dictionary<string, double> total = new Dictionary<string, double>();
+            total.Add("Grocery", 0);
+            total.Add("Amazon", 0);
+            total.Add("Cafe", 0);
+            total.Add("Zipcar", 0);
             Categories categories = new Categories();
             foreach (string[] fileCols in ColumnReader.CsvReadColumns(file, cols))
             {
                 foreach (string label in categories.Categorize(fileCols[0]))
                 {
-                    totalGr += double.Parse(fileCols[1].Replace("\"",""));
+                    total[label] += double.Parse(fileCols[1].Replace("\"",""));
                 }
             }
 
-            Console.WriteLine("Total groceries = " + totalGr);
+            Console.WriteLine("Totals ");
+            foreach (KeyValuePair<string,double> item in total)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
         }
 
     }
